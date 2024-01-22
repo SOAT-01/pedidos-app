66212c522eae7625f0bc22d9680d50580bd38177
HenriqueMitsuo <henrique@melhorplano.net>
2024-01-22 10:57:43 -0300

    refactor: update cliente and produto gateway implementation

--- src/controllers/pedido/factory.ts
+++ src/controllers/pedido/factory.ts
@@ -1,23 +1,19 @@
+import {
+    ClienteServiceGateway,
+    PedidoMongoGateway,
+    ProdutoServiceGateway,
+} from "gateways";
 import { PedidoUseCase } from "useCases";
-import { PedidoMongoGateway } from "gateways/pedidoGateway";
-import { ProdutoPostgresGateway } from "gateways/produtoGateway";
 import { PedidoModel } from "external/mongo/models";
-import { PostgresDB } from "external/postgres";
-import { ClienteSchema, ProdutoSchema } from "external/postgres/schemas";
+import { clienteServiceApi } from "external/clienteService";
+import { produtoServiceApi } from "external/produtoService";
 import { PedidoController } from "./controller";
-import { ClientePostgresGateway } from "gateways";
 
 export class PedidoControllerFactory {
     public static create(): PedidoController {
         const pedidoGateway = new PedidoMongoGateway(PedidoModel);
-        const produtoGateway = new ProdutoPostgresGateway(
-            PostgresDB,
-            ProdutoSchema,
-        );
-        const clienteGateway = new ClientePostgresGateway(
-            PostgresDB,
-            ClienteSchema,
-        );
+        const produtoGateway = new ProdutoServiceGateway(clienteServiceApi);
+        const clienteGateway = new ClienteServiceGateway(produtoServiceApi);
 
         const pedidoUseCase = new PedidoUseCase(
             pedidoGateway,
--- src/interfaces/gateways/produtoGateway.interface.ts
+++ src/interfaces/gateways/produtoGateway.interface.ts
@@ -1,10 +1,5 @@
-import { CategoriaEnum, Produto } from "entities/produto";
+import { Produto } from "entities/produto";
 
 export interface ProdutoGateway {
-    create(produto: Produto): Promise<Produto>;
-    getByCategoria(categoria: CategoriaEnum): Promise<Produto[]>;
-    getById(id: string): Promise<Produto | undefined>;
-    getByIds(ids: string[]): Promise<Produto[]>;
-    update(id: string, produto: Partial<Produto>): Promise<Produto>;
-    delete(id: string): Promise<void>;
+    getByIds(ids: string[]): Promise<Produto[] | undefined>;
 }
