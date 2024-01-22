7d9d13f3ea27eed17c96e8b2063ddeadf5b7080a
HenriqueMitsuo <henrique@melhorplano.net>
2024-01-22 10:59:21 -0300

    feat(config): add external services configs

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,32 +2,20 @@ import { parseEnvInt, parseEnvStr } from "./utils";
 
 export const serverConfig = {
     env: parseEnvStr("NODE_ENV", "development"),
-    port: parseEnvInt("PORT", 6001),
+    port: parseEnvInt("PORT", 6004),
     isProduction: process.env.NODE_ENV === "production",
     isDevelopment: process.env.NODE_ENV === "development",
+    clienteService: {
+        url: parseEnvStr("CLIENTE_SERVICE_URL", "http://localhost:6001/api/cliente"),
+    },
+    produtoService: {
+        url: parseEnvStr("PRODUTO_SERVICE_URL", "http://localhost:6002/api/produto"),
+    },
     mongo: {
         dbName: parseEnvStr("MONGO_DB_NAME", "fast_food"),
         connectionString: parseEnvStr(
             "MONGODB_CONN_STRING",
             "mongodb://localhost:27017",
-        ),
-    },
-    postgres: {
-        database: parseEnvStr("POSTGRES_DB", "fast_food"),
-        host: parseEnvStr("POSTGRES_DB_HOST", "127.0.0.1"),
-        port: parseEnvInt("POSTGRES_DB_PORT", 5432),
-        user: parseEnvStr("POSTGRES_DB_USER", "root"),
-        password: parseEnvStr("POSTGRES_DB_PASSWORD", "root"),
-        schemaFolder: "./src/external/postgres/schemas/*",
-        migrationFolder: "./src/external/postgres/migrations",
-    },
-    mercadoPago: {
-        apiUrl: "https://api.mercadopago.com",
-        userId: parseEnvStr("MERCADO_PAGO_USER_ID", "379061219"),
-        pos: parseEnvStr("MERCADO_PAGO_POS", "STORE001POS001"),
-        token: parseEnvStr(
-            "MERCADO_PAGO_TOKEN",
-            "TEST-2732348911740652-082612-1a10554b72bc3f53eab52110003023d0-379061219",
         ),
     },
 } as const;
