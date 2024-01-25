3118ab1416d3d0d73c1e37162ee80de7b21fedfe
HenriqueMitsuo <henrique@melhorplano.net>
2024-01-24 23:20:32 -0300

    fix(produtoServiceApi): use correct env variable

--- src/external/produtoService/api/index.ts
+++ src/external/produtoService/api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { serverConfig } from "config";
 
 export const produtoServiceApi = axios.create({
-    baseURL: serverConfig.clienteService.url,
+    baseURL: serverConfig.produtoService.url,
 });
 
 export type ProdutoServiceApi = typeof produtoServiceApi;
