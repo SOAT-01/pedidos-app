cd66a61a51c4dfd0fd065ee598d439bebdec349e
HenriqueMitsuo <henrique@melhorplano.net>
2024-02-25 15:23:37 -0300

    feat(config): add sqs and queues config

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,16 +6,40 @@ export const serverConfig = {
     isProduction: process.env.NODE_ENV === "production",
     isDevelopment: process.env.NODE_ENV === "development",
     clienteService: {
-        url: parseEnvStr("CLIENTE_SERVICE_URL", "http://localhost:6001/api/cliente"),
+        url: parseEnvStr(
+            "CLIENTE_SERVICE_URL",
+            "http://localhost:6001/api/cliente",
+        ),
     },
     produtoService: {
-        url: parseEnvStr("PRODUTO_SERVICE_URL", "http://localhost:6002/api/produto"),
+        url: parseEnvStr(
+            "PRODUTO_SERVICE_URL",
+            "http://localhost:6002/api/produto",
+        ),
     },
     mongo: {
         dbName: parseEnvStr("MONGO_DB_NAME", "fast_food"),
         connectionString: parseEnvStr(
             "MONGODB_CONN_STRING",
             "mongodb://localhost:27017",
+        ),
+    },
+    sqs: {
+        region: parseEnvStr("SQS_REGION", "us-east-1"),
+        accessKeyId: parseEnvStr("SQS_ACCESS_KEY_ID", "AKIASEKWN5VMCC3LDZNW"),
+        secretAccessKey: parseEnvStr(
+            "SQS_SECRET_ACCESS_KEY",
+            "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",
+        ),
+    },
+    queues: {
+        confirmacaoPagemento: parseEnvStr(
+            "QUEUE_CONFIRMACAO_PAGAMENTO",
+            "https://sqs.us-east-1.amazonaws.com/146747026776/confirmacao-pagamento",
+        ),
+        notificaoCliente: parseEnvStr(
+            "QUEUE_NOTIFICAO_CLIENTE",
+            "https://sqs.us-east-1.amazonaws.com/146747026776/notificacao-cliente",
         ),
     },
 } as const;
